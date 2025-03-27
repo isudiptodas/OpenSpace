ecd642565f4791f9ef9fbdd510166bc9f4b33d39
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-03-28 01:23:37 +0530

    Update landingPageFAQ.ts

--- src/data/landingPageFAQ.ts
+++ src/data/landingPageFAQ.ts
@@ -1,7 +1,7 @@
 export const faq = [
     {
       question: "What exactly is OpenSpace ?",
-      answer: `OpenSpace is a platform where users can look for several services which they want to book for themselves or even for their home services.
+      answer: `OpenSpace is a SaaS platform where users can look for several services which they want to book for themselves or even for their home services.
       In addition to this OpenSpace also has several sections like : \n crowdfunding where users can raise funds for a noble cause. \n Marketplace: where local buyers and sellers will connect to share goods and services promoting local businesses, \n Events: users can also organize any event in their common network  `
     },
 
@@ -18,5 +18,35 @@ export const faq = [
     {
       question: "Is OpenSpace free ?",
       answer: `Yes, for now OpenSpace is completely free to use for general public. `
+    },
+
+    {
+      question: "Are all the funding posts on OpenSpace genuine ?",
+      answer: `Yes, on OpenSpace all the funding posts which are listed on crowdfunding page are verified by our side and are all trusted. `
+    },
+
+    {
+      question: "Is OpenSpace marketplace only promotes new items ?",
+      answer: `Not exactly, in OpenSpace marketplace you can buy, sell, rent items which are new or even used. `
+    },
+
+    {
+      question: "Who can organize events through OpenSpace ?",
+      answer: `Well through OpenSpace anyone who is a user of this platform have the right to list an event and they can. `
+    },
+
+    {
+      question: "How to organize an event through OpenSpace ?",
+      answer: `As simple as sending a text message, first you need to fill out every basic detail about the event like : event name, event date, event vanue, event requirements and so on and after submitting we will review your event from our side and after successfull verification your event will be live on events page.`
+    },
+
+    {
+      question: "How much time does it takes to get a funding, event or goods approval on OpenSpace ?",
+      answer: `The entire verification process takes around 2 business days.`
+    },
+
+    {
+      question: "How will I know if my funding, event or goods verification gets rejected ?",
+      answer: `In case if we find any listing request that is against OpenSpace policy we have to remove it from the platform and in that case you will receive a mail on your registered email.`
     },
 ];
