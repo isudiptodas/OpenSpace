39646502f903a16b22a3e937787d7cd18a1f565a
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-03-28 20:51:19 +0530

    updated footer section with component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ import { faq } from "@/data/landingPageFAQ";
 import { AnimatedList } from "@/components/magicui/animated-list";
 import { Notification } from "@/components/Notification";
 import { notifications } from "@/data/notification";
+import Footer from '@/components/Footer';
 
 let newNotifications = notifications;
 newNotifications = Array.from({ length: 10 }, () => newNotifications).flat();
@@ -245,32 +246,7 @@ export default function Home() {
           </Accordion>
         </section>
 
-        <footer className="w-full h-72 xl:h-80 bg-black relative flex flex-col justify-start overflow-hidden items-center py-5 sm:gap-10">
-          <p className="w-auto py-1 px-5 rounded-full border-[1px] border-white mt-2 text-center text-white text-[12px] sm:text-lg xl:text-xl tracking-widest">OPENSPACE</p>
-
-          <div className="py-2 px-2 h-full mt-5 w-full grid grid-cols-3 justify-items-center">
-            
-            <div className="h-full w-full flex flex-col justify-start items-center">
-              <p className="text-white text-[12px] lg:text-lg font-bold mb-3">General</p>
-              <p className="text-white text-[10px] cursor-pointer lg:mb-1 lg:text-sm ">About Us</p>
-              <p className="text-white text-[10px] cursor-pointer lg:mb-1 lg:text-sm ">Why Us ?</p>
-              <p className="text-white text-[10px] cursor-pointer lg:mb-1 lg:text-sm ">Contact Us</p>
-            </div>
-            <div className="h-full w-full flex flex-col justify-start items-center">
-              <p className="text-white text-[12px] lg:text-lg font-bold mb-3">Services</p>
-              <p className="text-white text-[10px] cursor-pointer lg:mb-1 lg:text-sm ">Marketplace</p>
-              <p className="text-white text-[10px] cursor-pointer lg:mb-1 lg:text-sm ">Home Service</p>
-              <p className="text-white text-[10px] cursor-pointer lg:mb-1 lg:text-sm ">Crowdfunding</p>
-              <p className="text-white text-[10px] cursor-pointer lg:mb-1 lg:text-sm ">Events</p>
-            </div>
-            <div className="h-full w-full flex flex-col justify-start items-center">
-              <p className="text-white text-[12px] lg:text-lg font-bold mb-3">Website</p>
-              <p className="text-white text-[10px] cursor-pointer lg:mb-1 lg:text-sm ">Terms of Use</p>
-              <p className="text-white text-[10px] cursor-pointer lg:mb-1 lg:text-sm ">Privacy Policy</p>
-            </div>
-
-          </div>
-        </footer>
+        <Footer/>
 
 
       </div>
