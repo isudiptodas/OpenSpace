88220fa4f88f253ae7cf28c68368ea1eaa48eed5
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-03-30 00:25:23 +0530

    Update sidebarLinks.ts

--- src/data/sidebarLinks.ts
+++ src/data/sidebarLinks.ts
@@ -1,27 +1,29 @@
 
-export const sidebarLinks = [
-    {
-        label: "Services",
-        href: "/users/services",
-      },
-      {
-        label: "Marketplace",
-        href: "/users/marketplace",
-      },
-      {
-        label: "Crowdfunding",
-        href: "/users/crowdfunding",
-      },
-      {
-        label: "Events",
-        href: "/users/events",
-      },
-      {
-        label: "Profile",
-        href: "/users/events",
-      },
-      {
-        label: "Join Us",
-        href: "/users/events",
-      },
-]
+export const menuLinks = [
+  {
+    title: "Services",
+    iconSrc: "/images/menu/services.png", 
+    href: "/users/services",
+  },
+  {
+    title: "Marketplace",
+    iconSrc: "/images/menu/market.png",
+    href: "/users/marketplace",
+  },
+  {
+    title: "Crowdfunding",
+    iconSrc: "/images/menu/crowdfunding.png",
+    href: "/users/crowdfunding",
+  },
+  {
+    title: "Events",
+    iconSrc: "/images/menu/events.png",
+    href: "/users/events",
+  },
+  {
+    title: "Settings",
+    iconSrc: "/images/menu/settings.png",
+    href: "/users/settings",
+  },
+
+];
