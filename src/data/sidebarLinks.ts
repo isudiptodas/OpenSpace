698b458c1e89dd00e3cc3ec4d0b52f828c8e4b2a
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-03-29 21:52:45 +0530

    added sidebar links data

---
+++ src/data/sidebarLinks.ts
@@ -0,0 +1,27 @@
+
+export const sidebarLinks = [
+    {
+        label: "Services",
+        href: "/users/services",
+      },
+      {
+        label: "Marketplace",
+        href: "/users/marketplace",
+      },
+      {
+        label: "Crowdfunding",
+        href: "/users/crowdfunding",
+      },
+      {
+        label: "Events",
+        href: "/users/events",
+      },
+      {
+        label: "Profile",
+        href: "/users/events",
+      },
+      {
+        label: "Join Us",
+        href: "/users/events",
+      },
+]
