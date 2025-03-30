25457f6a5e44c7a5a334e295561aff1e1c51c35e
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-03-30 14:14:03 +0530

    Update marketplace page.tsx

--- src/app/users/marketplace/page.tsx
+++ src/app/users/marketplace/page.tsx
@@ -2,16 +2,36 @@
 
 import Link from "next/link";
 import Footer from '@/components/Footer';
+import { useState, useEffect } from "react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import { menuLinks } from "@/data/sidebarLinks";
 
 function page() {
+
+
     return (
         <>
-            <div className={`w-full min-h-screen flex flex-col justify-center items-center gap-5 relative`}>
+            <div className={`w-full duration-200 ease-in-out min-h-screen flex flex-col justify-start items-center gap-5 relative`}>
+                
+                {/* navbar */}
                 <div className={`w-full h-auto z-50 py-4 sm:py-5 backdrop-blur-3xl fixed top-0 flex justify-center items-center`}>
-                    <p className={`tracking-widest motion-preset-compress motion-duration-1500 text-sm font-light sm:text-[16px] lg:text-lg lg:font-medium`}>OPENSPACE</p>
+                    <p className={`tracking-widest text-sm font-light sm:text-[16px] lg:text-lg lg:font-medium duration-200 ease-in-out`}>OPENSPACE</p>
                 </div>
 
-                
+                {/* floating menu dock */}
+                <div className="w-full flex justify-end md:justify-center items-center fixed bottom-7 px-5">
+                    <div className=" w-auto ">
+                        <FloatingDock
+                            items={menuLinks}
+                        />
+                    </div>
+                </div>
+
+                {/* content section */}
+                <div className={`w-[95%] md:w-[90%] md:mt-20 mt-14 border-b-[1px] border-black shadow-2xl h-auto py-3 px-4 sm:px-5 sm:py-5 rounded-md lg:rounded-lg`}>
+                    <h1 className={`text-2xl sm:text-3xl`}>Marketplace</h1>
+                </div>
+
             </div>
 
             <Footer />
