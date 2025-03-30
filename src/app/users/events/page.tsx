0c1d35467763856078485558ddcce31721f948fb
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-03-30 15:22:03 +0530

    Update user events page.tsx

--- src/app/users/events/page.tsx
+++ src/app/users/events/page.tsx
@@ -12,7 +12,7 @@ function page() {
     return (
         <>
             <div className={`w-full duration-200 ease-in-out min-h-screen flex flex-col justify-start items-center gap-5 relative`}>
-                
+
                 {/* navbar */}
                 <div className={`w-full h-auto z-50 py-4 sm:py-5 backdrop-blur-3xl fixed top-0 flex justify-center items-center`}>
                     <p className={`tracking-widest text-sm font-light sm:text-[16px] lg:text-lg lg:font-medium duration-200 ease-in-out`}>OPENSPACE</p>
@@ -30,6 +30,10 @@ function page() {
                 {/* content section */}
                 <div className={`w-[95%] md:w-[90%] md:mt-20 mt-14 border-b-[1px] border-black shadow-2xl h-auto py-3 px-4 sm:px-5 sm:py-5 rounded-md lg:rounded-lg`}>
                     <h1 className={`text-2xl sm:text-3xl`}>Events</h1>
+                </div>
+
+                <div className={`w-[95%] md:w-[90%] py-3 px-4 h-auto grid grid-cols-1 md:grid-cols-3 justify-items-center gap-3`}>
+                   
                 </div>
 
             </div>
