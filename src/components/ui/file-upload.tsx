c86786ad2d6fbdf9cb9034969a80e0ef91843e13
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-03-31 21:47:13 +0530

    Updated file upload component height box

--- src/components/ui/file-upload.tsx
+++ src/components/ui/file-upload.tsx
@@ -92,7 +92,7 @@ export const FileUpload = ({
                   key={"file" + idx}
                   layoutId={idx === 0 ? "file-upload" : "file-upload-" + idx}
                   className={cn(
-                    "relative overflow-hidden z-40 bg-white dark:bg-neutral-900 flex flex-col items-start justify-start md:h-24 p-4 mt-4 w-full mx-auto rounded-md",
+                    "relative overflow-hidden z-40 bg-white dark:bg-neutral-900 flex flex-col items-start justify-start h-auto p-4 mt-4 w-full mx-auto rounded-md",
                     "shadow-sm"
                   )}
                 >
